f1da1a1a4a8d0e4914bf5f0aff256944f5d489b7
Deepanshu Chaid <bantar117@gmail.com>
2026-03-12 18:49:27 +0000

    USER REGISTER ROUTE COMPLETED

--- cine/cmd/main.go
+++ cine/cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	moviecontroller "github.com/DeepanshuChaid/Cine/tree/main/cine/internal/controllers/movieController"
+	usercontroller "github.com/DeepanshuChaid/Cine/tree/main/cine/internal/controllers/userController"
 	"github.com/DeepanshuChaid/Cine/tree/main/cine/internal/database"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -28,9 +29,13 @@ func main() {
 		c.String(200, "Hello World")
 	})
 
+	// MOVIE ROUTES
 	router.GET("/movies", moviecontroller.GetAllMovies())
 	router.GET("/movies/:id", moviecontroller.GetMovie())
 	router.POST("/create/movie", moviecontroller.CreateMovie())
+
+	// USER ROUTES
+	router.POST("/register", usercontroller.Register())
 
 	PORT := os.Getenv("PORT")
 
